f997e15ed67d52b773f708841a762ebad5c59e33
AlberGrana <134311582+AlberGrana@users.noreply.github.com>
2023-05-28 17:31:13 -0600

    Update Menu_Principal.c

--- Menu_Principal.c
+++ Menu_Principal.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+#include "Funciones.h"
 
 int main(int argc, char const* argv[])
 {
-    
+    int opcion = 0;
+    int bandera = 0;
+    while (!bandera)
+    {
+        opcion = menu();
+        CleanBuffer();
+        switch (opcion)
+        {
+        case 1:
+            CleanPantalla();
+            CleanBuffer();
+            int opcSMenu = 0;
+            opcSMenu = menuInventario;
+            CleanBuffer();
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        default:
 
+            break;
+        }
+    }   
     return 0;
 }
