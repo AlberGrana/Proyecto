ee3c62d7938967df3ca9091b07d752b74f6e7c95
AlberGrana <134311582+AlberGrana@users.noreply.github.com>
2023-05-29 18:42:01 -0600

    Update Menu_Principal.c

--- Menu_Principal.c
+++ Menu_Principal.c
@@ -1,39 +1,85 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <time.h>
-#include "Funciones.h"
+#include <string.h>
+#include "funciones.h"
+#include "Factura.h"
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
     int opcion = 0;
     int bandera = 0;
-    while (!bandera)
-    {
+    FILE *fileFact = NULL;
+
+    while(!bandera){
+
         opcion = menu();
         CleanBuffer();
-        switch (opcion)
+        int opcSMenu=0;
+        int bandera2 = 0;
+        switch (opcion) //switch principal
         {
-        case 1:
-            CleanPantalla();
-            CleanBuffer();
-            int opcSMenu = 0;
-            opcSMenu = menuInventario;
-            CleanBuffer();
-            break;
-        case 2:
-            /* code */
-            break;
-        case 3:
-            /* code */
-            break;
-        case 4:
-            /* code */
-            break;
-        default:
+            case 1:
+                while (!bandera2)
+                {
+                    bandera2 = 0;
+                    system("cls");
+                    CleanBuffer();
+                    opcSMenu = menuInventario();
+                    CleanBuffer();
+                    switch (opcSMenu)
+                    {
+                    case 1:
+                        system("cls");
+                        CleanBuffer();
+                        break;
+                    case 2:
+                        system("cls");
+                        CleanBuffer();
+                        break;
+                    case 3:
+                        system("cls");
+                        CleanBuffer();
+                        break;
+                    case 4:
+                        system("cls");
+                        bandera2 = 1;
+                        CleanBuffer();
+                        break;
+                    default:
+                        OpcionInvalida();
+                        break;
+                    }
+                }                
+                break;
+            
+            case 2:
+                while (!bandera2)
+                {
+                    bandera2 = 0;
+                    system("cls");
+                    CleanBuffer();
+                    opcSMenu = menuFacturacion();
+                    CleanBuffer();
+                    break;
+                }
+            
+            case 3:
+                system("cls");
+                CleanBuffer();
+                break;
+            
+            case 4: 
+                CleanBuffer();
+                SaltoLinea();   printf("Hasta pronto!!!");  SaltoLinea();
+                bandera = 1;
+                break;
 
-            break;
-        }
-    }   
+            default:
+                OpcionInvalida();
+                system("pause");
+                system("cls");
+                break;
+        } //cierra el switch
+    }
     return 0;
 }
