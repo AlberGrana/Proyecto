85e2bd0ddde41e05305f88af68adc89d433a0680
AlberGrana <134311582+AlberGrana@users.noreply.github.com>
2023-05-31 08:11:26 -0600

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -33,6 +33,43 @@ int menuInventario()
     printf("- Opcion 1: Visualizar Productos en Exitensia"); SaltoLinea();
     printf("- Opcion 2: Agregar Nuevo Producto"); SaltoLinea();
     printf("- Opcion 3: Editar Datos de un Producto"); SaltoLinea();
+    printf("- Opcion 4: Eliminar Producto"); SaltoLinea();
+    printf("- Opcion 5: Menu Proveedores"); SaltoLinea();
+    printf("- Opcion 6: Salir"); SaltoLinea();
+    printf("- Digite su opcion: ");
+    scanf("%d", &opc); CleanBuffer();
+    return opc;
+}
+int menuProducExistencia()
+{
+    int opc = 0;
+    printf("******************* Menu - Productos en Existensia *******************"); SaltoLinea(); SaltoLinea();
+    printf("- Opcion 1: Visualizar Todos los productos"); SaltoLinea();
+    printf("- Opcion 2: Buscar un producto"); SaltoLinea();
+    printf("- Opcion 3: Salir"); SaltoLinea();
+    printf("- Digite su opcion: ");
+    scanf("%d", &opc); CleanBuffer();
+    return opc;
+}
+int menuProveedores()
+{
+    int opc = 0;
+    printf("******************* Menu - Proveedores *******************"); SaltoLinea(); SaltoLinea();
+    printf("- Opcion 1: Visualizar Proveedores"); SaltoLinea();
+    printf("- Opcion 2: Agregar Nuevo Proveedor"); SaltoLinea();
+    printf("- Opcion 3: Editar Datos de Proveedor"); SaltoLinea();
+    printf("- Opcion 4: Eliminar Proveedor"); SaltoLinea();
+    printf("- Opcion 5: Salir"); SaltoLinea();
+    printf("- Digite su opcion: ");
+    scanf("%d", &opc); CleanBuffer();
+}
+int menuPacientes()
+{
+    int opc = 0;
+    printf("******************* Menu - Gestion de Pacientes *******************"); SaltoLinea(); SaltoLinea();
+    printf("- Opcion 1: Visualizar Productos en Exitensia"); SaltoLinea();
+    printf("- Opcion 2: Agregar Nuevo Producto"); SaltoLinea();
+    printf("- Opcion 3: Editar Datos de un Producto"); SaltoLinea();
     printf("- Opcion 4: Salir"); SaltoLinea();
     printf("- Digite su opcion: ");
     scanf("%d", &opc); CleanBuffer();
