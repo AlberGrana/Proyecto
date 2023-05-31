e7bd80d9f4361781c6a1d729dc9e8102090396c6
AlberGrana <134311582+AlberGrana@users.noreply.github.com>
2023-05-31 08:11:57 -0600

    Update Menu_Principal.c

--- Menu_Principal.c
+++ Menu_Principal.c
@@ -16,21 +16,46 @@ int main(int argc, char const *argv[])
         CleanBuffer();
         int opcSMenu=0;
         int bandera2 = 0;
+        int bandera3 = 0;
         switch (opcion) //switch principal
         {
-            case 1:
+            case 1://Menu Invetario
                 while (!bandera2)
                 {
-                    bandera2 = 0;
                     system("cls");
                     CleanBuffer();
                     opcSMenu = menuInventario();
                     CleanBuffer();
                     switch (opcSMenu)
                     {
-                    case 1:
-                        system("cls");
-                        CleanBuffer();
+                    case 1://Visualizar Productos en existencia
+                        while (!bandera3)
+                        {
+                            system("cls");
+                            CleanBuffer();
+                            opcSMenu = menuProducExistencia();
+                            CleanBuffer();
+                            switch (opcSMenu)
+                            {
+                            case 1: //Visualizar todos los productos
+                                system("cls");
+                                CleanBuffer();
+                                break;
+                            case 2: //Buscar un producto
+                                system("cls");
+                                CleanBuffer();
+                                break;
+                            case 3: //Salir
+                                system("cls");
+                                bandera3 = 1;
+                                CleanBuffer();
+                                break;
+                            default:
+                                OpcionInvalida();
+                                break;
+                            }
+                            CleanBuffer();
+                        }
                         break;
                     case 2:
                         system("cls");
@@ -41,6 +66,44 @@ int main(int argc, char const *argv[])
                         CleanBuffer();
                         break;
                     case 4:
+                        system("cls");
+                        CleanBuffer();                        
+                        break;
+                    case 5:
+                        while (!bandera2)
+                        {
+                            system("cls");
+                            CleanBuffer();
+                            opcSMenu = menuProveedores();
+                            switch (opcSMenu)
+                            {
+                            case 1:
+                                system("cls");
+                                CleanBuffer();
+                                break;
+                            case 2:
+                                system("cls");
+                                CleanBuffer();
+                                break;
+                            case 3:
+                                system("cls");
+                                CleanBuffer();
+                                break;
+                            case 4:
+                                system("cls");
+                                CleanBuffer();
+                                break;
+                            case 5:
+                                system("cls");
+                                bandera2 = 1;
+                                CleanBuffer();
+                                break;
+                            default:
+                                break;
+                            }
+                        }                        
+                        break;
+                    case 6:
                         system("cls");
                         bandera2 = 1;
                         CleanBuffer();
@@ -65,6 +128,7 @@ int main(int argc, char const *argv[])
             
             case 3:
                 system("cls");
+                opcSMenu = menuPacientes();
                 CleanBuffer();
                 break;
             
